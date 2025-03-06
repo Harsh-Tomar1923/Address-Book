072ca8e5b397ef9d40b61edcb6026b130c53e790
Harsh Tomar <harsh.thakur191003@gmail.com>
2025-03-06 21:06:21 +0530

    Add UC2

--- src/main/java/com/example/AddressBook/controller/AddressController.java
+++ src/main/java/com/example/AddressBook/controller/AddressController.java
@@ -3,12 +3,15 @@ package com.example.AddressBook.controller;
 import java.util.*;
 import com.example.AddressBook.dto.EmployeeDTO;
 import com.example.AddressBook.dto.ResponseDTO;
+import com.example.AddressBook.interfaces.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressController {
 
+    IEmployeeService iEmployeeService;
 
     //UC1 --> REST API's handled using responseDTO(without interference of service layer)
 
@@ -37,7 +40,32 @@ public class AddressController {
         return new ResponseDTO("API triggered at /res/delete/{id}", "Success");
     }
 
+    //UC2 --> Handling REST API's using Service layer without storing in DB
 
+    @GetMapping("/res2/get/{id}")
+    public ResponseDTO get2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/get/{id}", "Success");
+    }
+
+    @PostMapping("/res2/create")
+    public ResponseDTO create2(@RequestBody EmployeeDTO user){
+        return iEmployeeService.response("API triggered at /res/create", "Success");
+    }
+
+    @GetMapping("/res2/getAll")
+    public ResponseDTO getAll2(){
+        return iEmployeeService.response("API triggered at /res/getAll", "Success");
+    }
+
+    @PutMapping("/res2/edit/{id}")
+    public ResponseDTO edit2(@RequestBody EmployeeDTO user, @PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/edit/{id}", "Success");
+    }
+
+    @DeleteMapping("/res2/delete/{id}")
+    public ResponseDTO delete2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/delete/{id}", "Success");
+    }
 
 
 
---
+++ src/main/java/com/example/AddressBook/repositories/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.AddressBook.repositories;
+
+import com.example.AddressBook.entities.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+
+    public EmployeeEntity findByEmail(String email);
+
+}
